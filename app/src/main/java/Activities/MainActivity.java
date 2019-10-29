162ba4a220b75fe08f85e5f2e839092820992193
Hossain Ahmad <hossain98csedu@gmail.com>
2019-10-29 07:10:26 +0600

    Code Modularized

---
+++ app/src/main/java/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import csedu.englishlearningapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
